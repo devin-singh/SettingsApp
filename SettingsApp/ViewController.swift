d979588e43e5cfed2826bb2e3b2c46d9ef1361d8
Devin Singh <notpublic@gmail.com>
2020-01-13 10:18:39 -0700

    Project skeleton

---
+++ SettingsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by Devin Ghumman on 1/13/20.
+//  Copyright © 2020 Devin Ghumman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
