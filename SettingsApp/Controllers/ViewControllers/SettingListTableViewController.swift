952024f909dfd3a0588745ac4a25d4a5b06a0075
Devin Singh <notpublic@gmail.com>
2020-01-13 12:26:42 -0700

    Completed SettingListTableViewController with switches that change the bg color using protocol and delegate pattern

---
+++ SettingsApp/Controllers/ViewControllers/SettingListTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingListTableViewController.swift
+//  SettingsApp
+//
+//  Created by Devin Ghumman on 1/13/20.
+//  Copyright © 2020 Devin Ghumman. All rights reserved.
+//
+
+import UIKit
+
+class SettingListTableViewController: UITableViewController, SettingCellDelegate {
+
+    func settingSwitchTapped(for cell: SettingTableViewCell) {
+        
+        guard let indexPath = self.tableView.indexPath(for: cell) else {return}
+        
+        let setting = SettingController.sharedInstance.settings[indexPath.row]
+        
+        SettingController.sharedInstance.toggleIsOn(forSetting: setting)
+        
+        cell.updateViews(withSetting: setting)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return SettingController.sharedInstance.settings.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath) as? SettingTableViewCell else {return UITableViewCell()}
+        
+        let setting = SettingController.sharedInstance.settings[indexPath.row]
+        // Configure our cell
+        cell.updateViews(withSetting: setting)
+        
+        cell.cellDelegate = self
+        
+        return cell
+    }
+}
--- SettingsApp/Controllers/ViewControllers/SettingTableViewCell.swift
+++ SettingsApp/Controllers/ViewControllers/SettingTableViewCell.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
+protocol SettingCellDelegate: class {
+    func settingSwitchTapped(for cell: SettingTableViewCell)
+}
+
 class SettingTableViewCell: UITableViewCell {
     
     // Outlets
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var settingSwitch: UISwitch!
+
+    weak var cellDelegate: SettingCellDelegate?
     
     // Actions
     
+    func updateViews(withSetting setting: Setting) {
+        iconImageView.image = setting.icon
+        nameLabel.text = setting.name
+        settingSwitch.isOn = setting.isOn
+        
+        backgroundColor = settingSwitch.isOn ? .purple : .white
+        nameLabel.textColor = settingSwitch.isOn ? .white : .purple
+    }
     
+    @IBAction func settingSwitchToggled(_ sender: Any) {
+        cellDelegate?.settingSwitchTapped(for: self)
+    }
 }
 
 
